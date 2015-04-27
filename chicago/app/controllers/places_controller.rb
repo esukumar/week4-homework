049fb7b44a081b76fa8b57cf2b2d73a78754ac5b
Eric Sukumar <ericsukumar@gmail.com>
2015-04-27 00:02:00 -0500

    hw4 v1

--- chicago/app/controllers/places_controller.rb
+++ chicago/app/controllers/places_controller.rb
@@ -1,43 +1,49 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @places = Place.order('title asc')
   end
 
   def show
-    @place = Place.find(params[:id])
+    @place = Place.find_by(id: params[:id])
+    @reviews = Review.order('id desc').where(place_id: @place.id)
+    if @place == nil
+      redirect_to places_url, notice: "Place not found"
+    end
   end
 
   def new
+
   end
-
-  def submitted
-    Place.create title: params[:title],
-                 price: (params[:price].to_f * 100).to_i,
-                 photo_url: params[:url],
-                 desc: params[:desc]
-
-    redirect_to root_path
+  
+  def create
+    place = Place.new
+    place.title = params[:title]
+    place.price = (params[:price].to_f * 100).to_i
+    place.photo_url = params[:photo_url]
+    place.desc = params[:desc]
+    place.save
+    redirect_to places_url
   end
 
   def edit
-    @place = Place.find(params[:id])
+    @place = Place.find_by(id: params[:id])
   end
 
   def update
-    @place = Place.find(params[:id])
-    @place.update title: params[:title],
-                 price: (params[:price].to_f * 100).to_i,
-                 photo_url: params[:url],
-                 desc: params[:desc]
-
-    redirect_to "/places/#{@place.id}"
+    @place = Place.find_by(id: params[:id])
+    @place.title = params[:title]
+    @place.price = (params[:price].to_f * 100).to_i
+    @place.photo_url = params[:photo_url]
+    @place.desc = params[:desc]
+    @place.save
+    redirect_to place_url(params[:id])
   end
 
-  def delete
-    Place.delete(params[:id])
-    redirect_to root_path
+  def destroy
+    @place = Place.find_by(id: params[:id])
+    @place.delete
+    redirect_to places_url
   end
-
 
 end
--- chicago/app/controllers/reviews_controller.rb
+++ chicago/app/controllers/reviews_controller.rb
@@ -2,11 +2,11 @@ class ReviewsController < ApplicationController
 
   def create
     r = Review.new
-    r.place_id = params["place_id"]
-    r.rating = params["rating"]
-    r.desc = params["desc"]
+    r.place_id = params[:place_id]
+    r.rating = params[:rating]
+    r.desc = params[:desc]
     r.save
-    redirect_to "/places/#{r.place_id}"
+    redirect_to place_url(params[:place_id])
   end
 
 end
